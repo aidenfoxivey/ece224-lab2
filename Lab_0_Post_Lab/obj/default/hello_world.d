961a3250600b10aa3a9895df31fe3987c986b785
Mihir Gupta <msgupta@uwaterloo.ca>
2023-11-18 16:35:42 -0500

    Lab 2 Debounce

---
+++ Lab_0_Post_Lab/obj/default/hello_world.d
@@ -0,0 +1,27 @@
+obj/default/hello_world.o: hello_world.c \
+ ../Lab_0_Post_Lab_bsp//HAL/inc/sys/alt_irq.h \
+ ../Lab_0_Post_Lab_bsp//HAL/inc/nios2.h \
+ ../Lab_0_Post_Lab_bsp//HAL/inc/alt_types.h \
+ ../Lab_0_Post_Lab_bsp/system.h ../Lab_0_Post_Lab_bsp/linker.h \
+ ../Lab_0_Post_Lab_bsp//HAL/inc/priv/alt_legacy_irq.h \
+ ../Lab_0_Post_Lab_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../Lab_0_Post_Lab_bsp//HAL/inc/io.h \
+ ../Lab_0_Post_Lab_bsp//HAL/inc/alt_types.h
+
+../Lab_0_Post_Lab_bsp//HAL/inc/sys/alt_irq.h:
+
+../Lab_0_Post_Lab_bsp//HAL/inc/nios2.h:
+
+../Lab_0_Post_Lab_bsp//HAL/inc/alt_types.h:
+
+../Lab_0_Post_Lab_bsp/system.h:
+
+../Lab_0_Post_Lab_bsp/linker.h:
+
+../Lab_0_Post_Lab_bsp//HAL/inc/priv/alt_legacy_irq.h:
+
+../Lab_0_Post_Lab_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../Lab_0_Post_Lab_bsp//HAL/inc/io.h:
+
+../Lab_0_Post_Lab_bsp//HAL/inc/alt_types.h:
